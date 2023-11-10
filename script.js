d0b5a6b5af4a8bd4295314583ea72d07ef21cf23
Balghari <najmbalghari13@gmail.com>
2023-11-10 21:35:47 +0500

    basic form

---
+++ script.js
@@ -0,0 +1,51 @@
+// document.querySelector('form').addEventListener('click', (e)=>{
+//     e.preventDefault()
+// })
+
+const select = document.getElementById('select')
+const counter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const minusBtn = document.getElementById('minus')
+
+let count = 0
+addBtn.addEventListener('click', (e)=>{
+    e.preventDefault()
+    count=count+1  
+    counter.innerHTML = `${count}`
+    
+})
+minusBtn.addEventListener('click', (e)=>{
+    e.preventDefault()
+    if(count>0){
+        count = count-1
+        counter.innerHTML = `${count}`
+    }  
+})
+
+const submit = document.getElementById('submit')
+
+submit.addEventListener('click', (e)=>{
+     e.preventDefault()
+    let selectPhone = select.value
+    let selectColor = document.querySelector('input[name="color"]:checked').value;
+    if(selectColor.value===null){
+        alert('please select color')
+    }
+    
+    let selectAcces=[]
+    document.querySelectorAll('input[type="checkbox"]').forEach(items=>{
+        if(items.checked===true){
+            selectAcces.push(items.value)
+        }
+    })
+
+    document.querySelector('.result').innerHTML = `
+    <p>
+    You have selected ${count} ${selectPhone} phones having ${selectColor}
+    color and accessories ${selectAcces}
+    </p>
+    `
+   
+  
+})
+
